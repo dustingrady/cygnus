3f82deaf6665e781c53039646b907a0ee6d1c2db
Kellandor <ppham@csumb.edu>
2017-08-31 10:16:46 -0700

    Camera follow player after player moved certain distance

--- Assets/Scripts/MainCameraScript.cs
+++ Assets/Scripts/MainCameraScript.cs
@@ -30,6 +30,14 @@ public class MainCameraScript : MonoBehaviour {
     float yMin;
     [SerializeField]
     float smoothing = 0.5f;
+    [SerializeField]
+    Camera reference;
+
+    Vector3 oldPos;
+    Vector2 currentVelocity;
+
+    float allottedMovement = 100f;
+    bool testx, testy = false;
 
     Vector2 velocity = Vector2.zero;
     
@@ -37,22 +45,65 @@ public class MainCameraScript : MonoBehaviour {
     // Use this for initialization
     void Start () {
         target = GameObject.FindGameObjectWithTag("Player");
+        reference = GameObject.FindGameObjectWithTag("Reference").GetComponent<Camera>();
+        oldPos = Camera.main.WorldToScreenPoint(target.transform.position);
+        currentVelocity = target.GetComponent<Rigidbody2D>().velocity;
+        Debug.Log(oldPos);
 	}
 	
     void Update() {
+        currentVelocity = target.GetComponent<Rigidbody2D>().velocity;
+        if (currentVelocity.x == 0)
+        {
+            oldPos = Camera.main.WorldToScreenPoint(target.transform.position);
+        }
 
+        if ((Mathf.Abs(currentVelocity.x) >= 0 && (Mathf.Abs(currentVelocity.y) == 0)) &&
+            Mathf.Abs((Camera.main.WorldToScreenPoint(target.transform.position).x - oldPos.x)) >= allottedMovement)
+        {
+            //testx = true;
+            //testy = false;
+            float smoothx = Mathf.SmoothDamp(transform.position.x, target.transform.position.x, ref velocity.x, smoothing);
+            //float smoothy = Mathf.SmoothDamp(transform.position.y, target.transform.position.y, ref velocity.y, smoothing);
+
+            //transform.position = new Vector3(Mathf.Clamp(smoothx, xMin, xMax), Mathf.Clamp(smoothy, yMin, yMax), transform.position.z);
+            transform.position = new Vector3(Mathf.Clamp(smoothx, xMin, xMax), Mathf.Clamp(target.transform.position.y, yMin, yMax), transform.position.z);
+        }
+        else if ((Mathf.Abs(currentVelocity.x) >= 0 && (Mathf.Abs(currentVelocity.y) > 0)))
+        {
+            //testy = true;
+            //testx = false;
+            float smoothx = Mathf.SmoothDamp(transform.position.x, target.transform.position.x, ref velocity.x, smoothing);
+            float smoothy = Mathf.SmoothDamp(transform.position.y, target.transform.position.y, ref velocity.y, smoothing);
+
+            transform.position = new Vector3(Mathf.Clamp(smoothx, xMin, xMax), Mathf.Clamp(smoothy, yMin, yMax), transform.position.z);
+        }
+
+        Debug.Log((Camera.main.WorldToScreenPoint(target.transform.position).x - oldPos.x) + " " + currentVelocity.x);
     }
 
     // Late Update preferred so that we ensure target has moved before tracking
     void LateUpdate()
     {
-        float smoothx = Mathf.SmoothDamp(transform.position.x, target.transform.position.x, ref velocity.x, smoothing);
-        float smoothy = Mathf.SmoothDamp(transform.position.y, target.transform.position.y, ref velocity.y, smoothing);
+        /*
+        if (testx)
+        {
+            float smoothx = Mathf.SmoothDamp(transform.position.x, target.transform.position.x, ref velocity.x, smoothing);
+            //float smoothy = Mathf.SmoothDamp(transform.position.y, target.transform.position.y, ref velocity.y, smoothing);
 
-        transform.position = new Vector3(Mathf.Clamp(smoothx, xMin, xMax), Mathf.Clamp(smoothy, yMin, yMax), transform.position.z);
+            //transform.position = new Vector3(Mathf.Clamp(smoothx, xMin, xMax), Mathf.Clamp(smoothy, yMin, yMax), transform.position.z);
+            transform.position = new Vector3(Mathf.Clamp(smoothx, xMin, xMax), Mathf.Clamp(target.transform.position.y, yMin, yMax), transform.position.z);
+        }
+        if(testy)
+        {
+            float smoothx = Mathf.SmoothDamp(transform.position.x, target.transform.position.x, ref velocity.x, smoothing);
+            float smoothy = Mathf.SmoothDamp(transform.position.y, target.transform.position.y, ref velocity.y, smoothing);
+
+            transform.position = new Vector3(Mathf.Clamp(smoothx, xMin, xMax), Mathf.Clamp(smoothy, yMin, yMax), transform.position.z);
+        }
         //transform.position = new Vector3(Mathf.Clamp(target.transform.position.x, xMin, xMax), Mathf.Clamp(target.transform.position.y, yMin, yMax), transform.position.z);
         /*this.transform.position = new Vector3(target.transform.position.x + xOffset,
                                               target.transform.position.y + yOffset,
                                               target.transform.position.z + zOffset);*/
-    }
+        }
 }
