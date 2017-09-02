ea29c66cee3af695de943f1d9de908637a18282c
Kellandor <ppham@csumb.edu>
2017-09-02 12:08:16 -0700

    pre-merge commit

--- Assets/Scripts/TestCamScript.cs
+++ Assets/Scripts/TestCamScript.cs
@@ -35,14 +35,14 @@ public class TestCamScript : MonoBehaviour
         //Debug.Log(Camera.main.WorldToViewportPoint(target.transform.position).x + " "+ leftCameraBound);
         if(Camera.main.WorldToViewportPoint(target.transform.position).x <= (leftCameraBound - 0.01f))
         {
-            float camPos = target.transform.position.x + Camera.main.ViewportToWorldPoint(new Vector3(Mathf.Abs(Camera.main.WorldToViewportPoint(target.transform.position).x - rightCameraBound), 0, 0)).x;
+            float camPos = target.transform.position.x + Camera.main.ViewportToWorldPoint(new Vector3(Mathf.Abs(Mathf.Abs(Camera.main.WorldToViewportPoint(target.transform.position).x) - Mathf.Abs(rightCameraBound)), 0, 0)).x;
             float smoothx = Mathf.SmoothDamp(transform.position.x, target.transform.position.x, ref velocity.x, smoothing);
             transform.position = new Vector3(Mathf.Clamp(smoothx, xMin, xMax), Mathf.Clamp(target.transform.position.y, yMin, yMax), transform.position.z);
         }
 
         else if (Camera.main.WorldToViewportPoint(target.transform.position).x >= (rightCameraBound + 0.01f))
         {
-            float camPos = target.transform.position.x - Camera.main.ViewportToWorldPoint(new Vector3(Mathf.Abs(Camera.main.WorldToViewportPoint(target.transform.position).x - leftCameraBound), 0, 0)).x;
+            float camPos = target.transform.position.x - Camera.main.ViewportToWorldPoint(new Vector3(Mathf.Abs(Mathf.Abs(Camera.main.WorldToViewportPoint(target.transform.position).x) - Mathf.Abs(leftCameraBound)), 0, 0)).x;
             float smoothx = Mathf.SmoothDamp(transform.position.x, target.transform.position.x, ref velocity.x, smoothing);
             transform.position = new Vector3(Mathf.Clamp(smoothx, xMin, xMax), Mathf.Clamp(target.transform.position.y, yMin, yMax), transform.position.z);
         }
