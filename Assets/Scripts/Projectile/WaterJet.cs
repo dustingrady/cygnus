fa2840c4f4441bc96a5bfa9dfe7fcf7d1c2d28d7
Daniel <anzak06@gmail.com>
2017-10-13 09:18:37 -0700

    Improved projectile behavior and physics, added ice blocks, added extra tileset, and started on test level.

--- Assets/Scripts/Projectile/WaterJet.cs
+++ Assets/Scripts/Projectile/WaterJet.cs
@@ -3,20 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WaterJet : MonoBehaviour {
-	public float speed;
 	public Vector3 direction;
+	[SerializeField]
 
 	public void Initialize(Vector2 direction, float speed) {
-		this.speed = speed;
 		this.direction = direction.normalized;
+		gameObject.GetComponent<Rigidbody2D> ().AddForce (this.direction*speed);
 	}
 
+	/*
 	void Update() {
 		transform.position += direction * speed * Time.deltaTime;
 	}
+	*/
 
 	void OnTriggerEnter2D(Collider2D col) {
-		if (col.gameObject.tag != "Player") {
+		if (col.gameObject.tag != "Player" && col.gameObject.tag != "WaterElement" ) {
 			DestroyObject (this.gameObject);
 		}
 	}
