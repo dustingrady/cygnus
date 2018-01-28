1e996eff20c3fa15f989ec6991d7e252a0797c75
Dustin Grady <dustin.grady@gmail.com>
2018-01-28 02:00:40 -0800

    Added lava combo. Needing to add lava prefab and melting mechanic

---
+++ Assets/Scripts/Elements/Lava.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : Element{
+	public GameObject lava;
+
+	private bool btnReleased = true;
+	private GameObject plr;
+
+	public override void UseElement(Vector3 pos, Vector2 dir){
+		if (btnReleased){
+			GameObject steamObject = Instantiate(lava, pos, Quaternion.identity);
+
+			steamObject.GetComponent<ParticleSystem>().Play();
+
+			// Destroy the system after it finishes
+			//Destroy(steamObject, 1f);
+
+			Debug.Log("Shooting lava");
+			btnReleased = false;
+		}
+	}
+
+	void Start(){
+
+	}
+
+	void Update(){
+		if (Input.GetMouseButtonUp(2) == true){
+			btnReleased = true;
+		}
+	}
+}
