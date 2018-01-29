121cb23626c216a913ea14b144074fc505052a81
Dustin Grady <dustin.grady@gmail.com>
2018-01-28 21:21:11 -0800

    Added terrible looking lava prefab

--- Assets/Scripts/Elements/Lava.cs
+++ Assets/Scripts/Elements/Lava.cs
@@ -2,33 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lava : Element{
-	public GameObject lava;
-
-	private bool btnReleased = true;
-	private GameObject plr;
+public class Lava : Element {
+	public GameObject lavaJet;
+	[SerializeField]
+	private float lavaJetCooldown = 0.1f;
+	[SerializeField]
+	private float jetStrength = 500;
+	private float timeSinceFire;
 
 	public override void UseElement(Vector3 pos, Vector2 dir){
-		if (btnReleased){
-			GameObject steamObject = Instantiate(lava, pos, Quaternion.identity);
-
-			steamObject.GetComponent<ParticleSystem>().Play();
-
-			// Destroy the system after it finishes
-			//Destroy(steamObject, 1f);
-
-			Debug.Log("Shooting lava");
-			btnReleased = false;
+		if (timeSinceFire > lavaJetCooldown) {
+			GameObject fb = Instantiate (lavaJet, pos, Quaternion.identity);
+			fb.GetComponent<WaterJet> ().Initialize (dir, jetStrength);
+			timeSinceFire = 0;
 		}
 	}
 
-	void Start(){
-
-	}
-
-	void Update(){
-		if (Input.GetMouseButtonUp(2) == true){
-			btnReleased = true;
-		}
+	void Update() {
+		timeSinceFire += Time.deltaTime;
 	}
 }
