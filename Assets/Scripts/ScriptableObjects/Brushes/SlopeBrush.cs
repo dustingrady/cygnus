675b68258e4cf4d3ae1d8e1f5e4348909ce832fb
John Sullivan <jsullivan@csumb.edu>
2018-02-13 19:19:18 -0800

    Trying to get custom collisions to work

---
+++ Assets/Scripts/ScriptableObjects/Brushes/SlopeBrush.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEditor;
+
+[CustomGridBrush(true, false, false, "Slope Brush")]
+public class SlopeBrush : GridBrush {
+    /*
+    public override void Paint(GridLayout gridLayout, GameObject brushTarget, Vector3Int position)
+    {
+        base.Paint(gridLayout, brushTarget, position);
+        var tilemap = brushTarget.GetComponent<Tilemap>();
+        var collider = tilemap.
+    }
+    */
+
+    public override void Pick(GridLayout gridLayout, GameObject brushTarget, BoundsInt position, Vector3Int pickStart)
+    {
+        base.Pick(gridLayout, brushTarget, position, pickStart);
+
+        foreach (var cell in cells) {
+            var cellTile = (Tile)cell.tile;
+            //cellTile.colliderType = Tile.ColliderType.Sprite;
+        }
+    }
+}
+
+[CustomEditor(typeof(SlopeBrush))]
+public class SlopeBrushEditor : GridBrushEditor { }
---
+++ Assets/Scripts/ScriptableObjects/Brushes/SlopeTile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class SlopeTile : TileBase {
+    TileData data = new TileData();
+    public Tile orig;
+
+    public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+    {
+        orig.GetTileData(position, tilemap, ref data);
+        /*
+        var tileObject = data.gameObject;
+
+        Debug.Log(tileObject.GetComponent<PolygonCollider2D>());
+
+        var collider = tileObject.GetComponent<PolygonCollider2D>();
+        collider.CreatePrimitive(3);
+
+        tileData = data;
+        */
+    }
+
+    public override void RefreshTile(Vector3Int position, ITilemap tilemap)
+    {
+        orig.RefreshTile(position, tilemap);
+    }
+}
