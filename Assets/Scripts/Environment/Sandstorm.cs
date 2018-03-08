e1818e3973f92fe11dc51444287af34b10b77302
unknown <tchargin@csumb.edu>
2018-03-08 14:51:47 -0800

    Added Desert Level, Temp (Disabled) Quicksand, Temp (Disabled (Sandstorm) Sandstorm is activating force on player being fucky fix soon.

---
+++ Assets/Scripts/Environment/Sandstorm.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sandstorm : MonoBehaviour {
+
+    public ParticleSystem particleSystem;
+    private Rigidbody2D playerRigidbody2D;
+
+    // Use this for initialization
+    void Start () {
+        var player = GameObject.FindWithTag("Player");
+        playerRigidbody2D = player.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (particleSystem.isStopped) {
+            //playerRigidbody2D.AddForce(new Vector2(-20, 0));
+        }
+    }
+}
---
+++ Assets/Scripts/Platforms/Quicksand.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Quicksand : MonoBehaviour
+{
+
+    private Tilemap tilemap;
+    List<Tile> prevTile = new List<Tile>();
+    List<Vector3> tilesHit;
+    TileBase previousTile;
+    Vector3 prevBlock;
+
+    // Use this for initialization
+    void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+
+        }
+
+        if (collision.gameObject.tag == "Enemy")
+        {
+
+        }
+    }
+}
