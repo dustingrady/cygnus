6ca0a49bc558cd94efdd0f3bc7a5f3d0be921841
kellandor <kellandor@gmail.com>
2018-03-28 09:55:46 -0700

    Particles for potions.

--- Assets/Scripts/ScriptableObjects/Items/Potion.cs
+++ Assets/Scripts/ScriptableObjects/Items/Potion.cs
@@ -8,11 +8,14 @@ public class Potion : Item {
 	public float healAmount;
 	public string description;
 	private bool consumable = true;
+	GameObject ps;
 
 	public override void useItem()
 	{
+		ps = Resources.Load ("Prefabs/Particles/HealingEffect") as GameObject;
 		player = GameObject.Find ("Player").GetComponent<Player> ();
 		player.healWounds (healAmount);
+		Instantiate (ps, player.transform.position, Quaternion.identity);
 		Debug.Log (player.health.CurrentVal);
 	}
 
