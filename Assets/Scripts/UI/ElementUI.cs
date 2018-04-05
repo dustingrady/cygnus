ffcef6d3de0d51acbe807e1ceda3a3b5f324a7df
Daniel <anzak06@gmail.com>
2018-04-05 10:21:22 -0700

    Expanded forest level Simplified Playground Scene Fixed Ruins scene other stuff I forgot ¯\_(ツ)_/¯

--- Assets/Scripts/UI/ElementUI.cs
+++ Assets/Scripts/UI/ElementUI.cs
@@ -76,38 +76,26 @@ public class ElementUI : MonoBehaviour {
 
 	public void EnableElements() {
 		Transform leftElement = transform.Find ("LeftElement");
-		leftElementImg = leftElement.Find ("Icon").GetComponent<Image> ();
-		leftElementImgCd = leftElement.Find ("Icon").Find ("IconCD").GetComponent<Image> ();
-
 		Transform rightElement = transform.Find ("RightElement");
-		rightElementImg = rightElement.Find ("Icon").GetComponent<Image> ();
-		rightElementImgCd = leftElement.Find ("Icon").Find ("IconCD").GetComponent<Image> ();
-
 		Transform centerElement = transform.Find ("CenterElement");
-		centerElementImg = centerElement.Find ("Icon").GetComponent<Image> ();
-		centerElementImgCd = leftElement.Find ("Icon").Find ("IconCD").GetComponent<Image> ();
 
 		leftElement.transform.localScale = Vector3.one;
 		rightElement.transform.localScale = Vector3.one;
 		centerElement.transform.localScale = Vector3.one;
+
+		UpdateElements ();
 	}
 
 
 	public void DisableElements() {
 		Transform leftElement = transform.Find ("LeftElement");
-		leftElementImg = leftElement.Find ("Icon").GetComponent<Image> ();
-		leftElementImgCd = leftElement.Find ("Icon").Find ("IconCD").GetComponent<Image> ();
-
 		Transform rightElement = transform.Find ("RightElement");
-		rightElementImg = rightElement.Find ("Icon").GetComponent<Image> ();
-		rightElementImgCd = leftElement.Find ("Icon").Find ("IconCD").GetComponent<Image> ();
-
 		Transform centerElement = transform.Find ("CenterElement");
-		centerElementImg = centerElement.Find ("Icon").GetComponent<Image> ();
-		centerElementImgCd = leftElement.Find ("Icon").Find ("IconCD").GetComponent<Image> ();
 
 		leftElement.transform.localScale = Vector3.zero;
 		rightElement.transform.localScale = Vector3.zero;
 		centerElement.transform.localScale = Vector3.zero;
+
+		UpdateElements ();
 	}
 }
