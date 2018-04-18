34cd9211dbbae84e5cb40a210a4b80abf17f919d
Scott Grether <sgrether@csumb.edu>
2018-04-17 18:29:50 -0700

    Started work on acid boss. Camera swapping currently working. Damage to boss currently not working.

--- Assets/Scripts/Enemies/Bosses/AcidBossTest2.cs
+++ Assets/Scripts/Enemies/Bosses/AcidBossTest2.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AcidBossTest2 : MonoBehaviour {
+public class AcidBossTest2 : Enemy {
+	private float radius = 40.0f; //How far we can see player
+	private bool shootingPlayer;
+	private EnemyShooting es;
+
+	private CameraSwitch cs;
+
+	private void Awake(){
+		es = gameObject.GetComponent<EnemyShooting> ();
+	}
 
 	// Use this for initialization
 	void Start () {
-		
+		cs = GameObject.Find ("CameraSwapTrigger").GetComponent<CameraSwitch> ();
+		base.Start (); // Call the based enemy Start() function	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (this.hitpoints <= 0) {
+			cs.playerCam = true;
+		}
+		EvaluateHealth ();
+
+		if (DistanceToPlayer() <= radius) {
+			es.shoot_At_Player ();
+			//takeDamage (0.01f);
+		}
 	}
+
 }
+
---
+++ Assets/Scripts/Events/AcidFlats/CameraSwitch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour {
+	public bool playerCam = true;
+	private GameObject targetCM;
+	private GameObject playerCM;
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.gameObject.tag == "Player") {
+			playerCam = false;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		targetCM = GameObject.Find("BossCamera");
+		playerCM = GameObject.Find ("Follow CM");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerCam == false && GameObject.Find("snek") != null) {
+			playerCM.SetActive (false);
+			targetCM.SetActive (true);
+		} else if (playerCam == true) {
+			targetCM.SetActive (false);
+			playerCM.SetActive (true);
+		}
+	}
+}
