3fd082b91f71025782df8a99c972fd77987c242a
kellandor <kellandor@gmail.com>
2018-05-08 15:24:59 -0700

    Hid boss entrance.

---
+++ Assets/Scripts/Utility/ShowBossEntrance.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowBossEntrance : MonoBehaviour {
+
+	public GameObject area1;
+	public GameObject area2;
+
+	public GameObject bossEntrance;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (area1.GetComponent<Triggered> ().touched && area2.GetComponent<Triggered> ().touched) {
+			if (!bossEntrance.activeInHierarchy) {
+				bossEntrance.SetActive (true);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Utility/Triggered.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triggered : MonoBehaviour {
+
+	public bool touched = false;
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player") {
+			touched = true;
+		}
+	}
+}
