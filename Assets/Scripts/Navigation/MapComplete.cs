b99215759a890743d932b0f885bc6132aac6f821
Daniel <anzak06@gmail.com>
2018-05-12 21:02:17 -0700

    Added crosses to completed levels on the map.

---
+++ Assets/Scripts/Navigation/MapComplete.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapComplete : MonoBehaviour {
+
+	public GameObject ruinsCheck;
+	public GameObject forestCheck;
+	public GameObject acidCheck;
+	public GameObject desertCheck;
+	public GameObject phucCheck;
+
+	// Use this for initialization
+	void Start () {
+		GameManager gm = GameManager.instance;
+
+		if (gm.CheckQuestComplete (0)) {
+			ruinsCheck.SetActive (true);	
+		}
+		if (gm.CheckQuestComplete (199)) {
+			forestCheck.SetActive (true);	
+		}
+		if (gm.CheckQuestComplete (299)) {
+			acidCheck.SetActive (true);	
+		}
+		if (gm.CheckQuestComplete (399)) {
+			desertCheck.SetActive (true);	
+		}
+		if (gm.CheckQuestComplete (499)) {
+			phucCheck.SetActive (true);	
+		}
+	}
+}
