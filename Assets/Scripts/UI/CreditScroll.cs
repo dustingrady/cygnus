32b188215177c24c2eb7f09233e4eb1fb46681d2
Daniel <anzak06@gmail.com>
2018-05-13 02:33:58 -0700

    Added credits to menu and cleaned up unused files.

---
+++ Assets/Scripts/UI/CreditScroll.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditScroll : MonoBehaviour {
+
+	public bool scrolling;
+	public GameObject cam;
+	public GameObject MenuUI;
+	public float endPosX = 101;
+	public float speed = 2;
+
+	void Start () {
+		cam = Camera.main.gameObject;
+	}
+	
+	void Update () {
+		if (scrolling) {
+			cam.transform.Translate (new Vector3 (Time.deltaTime * speed, 0, 0));
+
+			if (cam.transform.position.x >= endPosX) {
+				scrolling = false;
+				StartCoroutine (ReloadScene ());
+			}
+		}
+	}
+
+	public void RollCredits() {
+		MenuUI.SetActive (false);
+		scrolling = true;
+	}
+
+	IEnumerator ReloadScene() {
+		yield return new WaitForSeconds (5f);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+}
